5cb47a8f7245d54f340537021be90c7d8dc9b12e
Midhun Nirmal <midhunnirmal123@gmail.com>
2022-11-13 17:05:43 +0530

    this is added at functions

---
+++ helloworld.js
@@ -0,0 +1,14 @@
+// let myname = "World";
+// console.log("hello world,"+myname);
+// food =Number(prompt ('how much the food cost'));
+// tippercent=Number(prompt('the tip percent'));
+// console.log(food);
+// tipcost = Number((food*tippercent));
+// totalcost= Number(food+tipcost);
+// console.log(totalcost);
+// fruit = prompt('what is your favorite fruiit');
+// console.log(fruit);
+
+let num = Math.floor(Math.random()*7);
+
+console.log(num);
